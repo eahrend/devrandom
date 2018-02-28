05b3b4d643b03fb10b2f372c078d124a9f47f671
eahrend <30596165+eahrend@users.noreply.github.com>
2018-02-27 21:46:05 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,3 +1,68 @@
 package main
 
-// initial commit to pull down stuff
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+
+	"crypto/sha256"
+
+	cpu "github.com/shirou/gopsutil/cpu"
+)
+
+// Seeds the thread with a combination of time now and the pid
+func getSeed() {
+	pid := os.Getpid()
+	var pid64 int64
+	pid64 = int64(pid)
+	timeNow := time.Now().UTC().UnixNano()
+	seedInt := pid64 + timeNow
+	rand.Seed(seedInt)
+}
+
+// Used to get a random int.
+func getRandomInt(min, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+// Actually get the random output
+func getEntropy() ([]byte, error) {
+	d, err := time.ParseDuration("1s")
+	if err != nil {
+		return nil, err
+	}
+	info, err := cpu.Percent(d, false)
+	if err != nil {
+		return nil, err
+	}
+	var infoTotal int
+	for i := range info {
+		usage := info[i]
+		var intVal int
+		intVal = int(usage)
+		infoTotal = infoTotal + intVal
+	}
+	hash := sha256.New()
+	infoString := strconv.Itoa(infoTotal)
+	randInt := getRandomInt(0, 99999999)
+	intString := strconv.Itoa(randInt)
+	randString := intString + infoString
+	hash.Write([]byte(randString))
+	entropyValue := hash.Sum(nil)
+	return entropyValue, nil
+}
+
+// Keep running until we exit
+func main() {
+	for {
+		output, err := getEntropy()
+		if err != nil {
+			fmt.Println("Error occured getting entropy:", err)
+			os.Exit(1)
+		}
+		fmt.Println(string(output))
+		getSeed()
+	}
+}
